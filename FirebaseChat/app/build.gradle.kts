8e31d377ed9f7d36c1e5694c48be76007144819f
Jinggu0_ <112626799+yoonhyuk1208@users.noreply.github.com>
2026-06-05 00:04:27 +0900

    Connect with Firebase

--- FirebaseChat/app/build.gradle.kts
+++ FirebaseChat/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -38,6 +39,7 @@ android {
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
